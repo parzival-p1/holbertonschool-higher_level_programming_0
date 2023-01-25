947776d4d0097efe231855ed844bd2fdd63d81ae
Paco Hdez <francj.hdez@gmail.com>
2023-01-24 19:59:54 -0600

    Write a program that prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv):
+        print("0 aguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for argument in range(1, len(argv)):
+            print("{}: {}".format(argument, argv[argument]))
