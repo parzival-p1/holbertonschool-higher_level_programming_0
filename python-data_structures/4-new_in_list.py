d6e9a9843d0dbed8a407ce13b3f01ad5c300ce50
Paco Hdez <francj.hdez@gmail.com>
2023-01-27 23:57:23 -0600

    Write a function that replaces an element in a list at a specific position without modifying the original list (like in C)

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
     dup = my_list[:]
     if idx >= 0 and idx < len(dup):
         dup[idx] = element
