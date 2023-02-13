3a9fe6ea59bda52e576df3b352d2075d5cc77bb0
Paco Hdez <francj.hdez@gmail.com>
2023-02-13 15:08:17 -0600

    Divides 2 integers and prints the result.

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,9 +3,9 @@ def safe_print_division(a, b):
     """ divides 2 integers and prints the result """
     try:
         div = a / b
-        print("Inside Result: {:.1f}".format(div))
     except ZeroDivisionError:
         div = None
-        print("Inside Result: {}".format(div))
+        return None
     finally:
-        return div
+        print("Inside result: {}".format(div))
+        return (div)
