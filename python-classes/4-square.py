63023d894bb1548b5599267a94a4cec45696a6b9
Paco Hdez <francj.hdez@gmail.com>
2023-02-21 22:57:32 -0600

    Write a class Square that defines a square by: (based on 3-square.py). SETTER

---
+++ python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 3-square.py) """
+
+
+class Square:
+    """ Defines a Square """
+
+    def __init__(self, size=0):
+        """ Create a square.
+        Args:
+            size: size of the square.
+        Raises:
+            TypeError: If size is not integer.
+            ValueError: If size is lower than zero.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Get the area of a Square.
+        Returns: Area of the square instance.
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ size: Size of the square. Function Getter """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """size: Size of the square. Function Setter.
+        Raises:
+            TypeError: If size is not integer.
+            ValueError: If size is lower than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
